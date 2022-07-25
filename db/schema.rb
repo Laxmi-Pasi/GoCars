5fd0780c76ac3ef3b3e54e2ab3a3fb2f04413774
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-07-25 16:27:18 +0530

    Style: UNT-T9484 change navbar styling and give user conditions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_110358) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_22_082410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,9 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_110358) do
     t.string "registered_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_082410) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
