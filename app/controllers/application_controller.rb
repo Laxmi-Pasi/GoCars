eb1c4df837f37598d7242834daf348bb0d0f5428
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-07-26 19:23:46 +0530

    feat: change all layout for user and car model, add validation for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters_for_update, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact])
+  end
+
+  def configure_permitted_parameters_for_update
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :contact])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable      
+  validates :first_name,:last_name, presence: true, length: {minimum:3 , maximum:25}
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\W]).{8,}\z/i
+  validates :email, presence: true,uniqueness: {case_sensitive: false}, length: {maximum:105}, format: { with: VALID_EMAIL_REGEX}
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, if: :skip_validation
+  validates :contact,:presence => true,
+            :numericality => true,
+            :length => { :minimum => 10, :maximum => 15 }
+  before_update: :skip_validation
+  def skip_validation
+    if self.password == nil
+      return false
+    else
+      return true
+    end
+  end
 end
