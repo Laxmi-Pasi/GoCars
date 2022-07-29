6d30239219e74773eda5f8f188f8e0b04f2ff6e2
Nikunj Purohit <nikunj.p@simformsolutions.com>
2022-07-29 17:45:32 +0530

    feat: added sell price and rent price

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  User.create(first_name: "User ##{i}", last_name: "User ##{i}", email:"user#{i}@user.user", password: "123456", password_confirmation: "123456")
+end
+
+5.times do |i|
+  Car.create(coma)
+end
