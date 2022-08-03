328120a560f4630f721837c22bdfbba74c332885
Nikunj Purohit <nikunj.p@simformsolutions.com>
2022-08-03 13:29:28 +0530

    implemented changes suggested by reviewer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact])
   end
